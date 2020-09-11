1390d441be73e7229e0ed51314205a5434fa6187
wangkun <1449377343@qq.com>
2020-09-11 17:11:16 +0800

    学习队列

--- src/main/java/com/study/practice/springboot/entityclass/thread/pool/queue/LinkedBlockingDequeTest.java
+++ src/main/java/com/study/practice/springboot/entityclass/thread/pool/queue/LinkedBlockingDequeTest.java
@@ -2,13 +2,12 @@ package com.study.practice.springboot.entityclass.thread.pool.queue;
 
 import com.study.practice.springboot.entityclass.thread.thread.MyRunnable3;
 
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class LinkedBlockingDequeTest {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         ThreadPoolExecutor executor =
                 new ThreadPoolExecutor(1, 1, 10000L,
                         TimeUnit.MICROSECONDS, new LinkedBlockingDeque<Runnable>(2),
@@ -18,5 +17,6 @@ public class LinkedBlockingDequeTest {
             executor.execute(new MyRunnable3(i, "thread name is:" + i));
             System.out.println("当前线程池的数量为：" + executor.getPoolSize());
         }
+        executor.shutdown();
     }
 }
