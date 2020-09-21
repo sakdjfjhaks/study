94c496d028e3e9ff08820cc505bdb5cd7662d740
wangkun <1449377343@qq.com>
2020-09-21 14:42:36 +0800

    rabbitmq学习

--- src/main/java/com/study/practice/springboot/entityclass/rabbitmq/SendMessageController.java
+++ src/main/java/com/study/practice/springboot/entityclass/rabbitmq/SendMessageController.java
@@ -1,18 +1,10 @@
 package com.study.practice.springboot.entityclass.rabbitmq;
 
 import com.study.practice.base.service.DirectRabbitService;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 /**
  * @Author : JCccc
  * @CreateTime : 2019/9/3
